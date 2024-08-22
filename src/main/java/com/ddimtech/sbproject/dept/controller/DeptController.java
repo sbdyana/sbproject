25b87e91e7360cab0fa5f90f3c402cb809ee84ff
sbdyana <sbdyana@github.com>
2024-08-22 14:58:49 +0900

    Deptcontroller 정리

--- src/main/java/com/ddimtech/sbproject/dept/controller/DeptController.java
+++ src/main/java/com/ddimtech/sbproject/dept/controller/DeptController.java
@@ -15,18 +15,30 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 // rest용 controller로 설정
+// 특정 uri로 요청을 보내면 Controller에서 어떠한 방식으로 처리할지를 정의한다.
+// ResponseBody를 포함한다.
 @RestController
+
 // 이 클래스의 공통 prefix uri 설정
 @RequestMapping("sample/dept")
+
 // swagger 에 표시 될 설명
+// 어노테이션은 API 엔드포인트에 태그를 할당하여 관련된 엔드포인트를 그룹화하고 문서에서 카테고리를 형성하는 데 사용된다. 따라서 주로 @RestController가 붙어있는 클래스에 사용된다.
 @Tag(name = "Dept", description = "Dept API")
+
 // private final 로 선언된 object를 autowire 함
+// 새로운 필드를 추가할 때 다시 생성자를 만들어서 관리해야하는 번거로움을 없애준다. (@Autowired를 사용하지 않고 의존성 주입)
 @RequiredArgsConstructor
 public class DeptController {
 
+    // service에서 비즈니스 로직 처리
+    // Controller는 Service에 의존한다. service는 생성자를 통해 주입 된다.
+    // Controller는 HTTP 요청 처리와 응답 생성에 집중하고 실제 사용자 조회 로직은 Service에 위임한다.
     private final DeptService deptService;
 
     @GetMapping
+    // API 엔드포인트의 작업에 대한 설명을 추가하고 세부 정보를 제공하는 데 사용된다. 주로 @RestController 클래스 내부의 메서드에 사용된다.
+    // summary를 통해 작업의 요약, description을 통해 작업의 구체적인 설명을 작성할 수 있다. 작업에 대한 정보를 명시적으로 제공함으로 API 문서의 가독성과 이해도를 향상할 수 있다.
     @Operation(summary = "dept 리스트")
     public CommonResult list(DeptSearchReqDto deptSearchReqDto) {
         return deptService.list(deptSearchReqDto);
