45cafa14be16e719cc5caf0b3aa166624fa703e0
sbdyana <tnqls9301@naver.com>
2024-08-26 17:26:00 +0900

    deptCreatedReqDto 정리 전

--- src/main/java/com/ddimtech/sbproject/dept/dto/DeptCreateReqDto.java
+++ src/main/java/com/ddimtech/sbproject/dept/dto/DeptCreateReqDto.java
@@ -12,6 +12,7 @@ import org.hibernate.validator.constraints.Length;
 public class DeptCreateReqDto {
     @NotBlank
     @Length(min = 1, max = 20, message = "최소 길이 1, 최대 길이 20")
+    // 어노테이션은 API 모델의 속성을 정의하고 문서화하는 데 사용된다. 다시 말해서 요청과 응답에 사용되는 DTO 클래스나 필드에 사용할 수 있다.
     @Schema(description = "부서명", defaultValue = " ")
     private String dname;
 
