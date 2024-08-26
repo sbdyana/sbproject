f90f513ce9ea17470659ae453c4326481f16a3b7
sbdyana <tnqls9301@naver.com>
2024-08-26 17:25:14 +0900

    main // 추가

--- src/main/java/com/ddimtech/sbproject/SbprojectApplication.java
+++ src/main/java/com/ddimtech/sbproject/SbprojectApplication.java
@@ -11,6 +11,9 @@ import org.springframework.context.ApplicationContext;
 @RequiredArgsConstructor
 public class SbprojectApplication {
 
+    //Spring의 ApplicationContext를 클래스의 필드로 선언
+    //final로 필드가 한 번 초기화되면 그 후에 변경될 수 없음
+    //ApplicationContext는 Spring Framework의 핵심 인터페이스 중 하나로 Application의 구성 정보를 제공하는 중앙 인터페이스 역할을 함.
     private final ApplicationContext context;
 
     public static void main(String[] args) {
