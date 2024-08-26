980c00d33208c74177363949975ab5b694be7d23
sbdyana <tnqls9301@naver.com>
2024-08-26 22:40:27 +0900

    enum 정리중

--- src/main/java/com/ddimtech/sbproject/common/CommonResultCode.java
+++ src/main/java/com/ddimtech/sbproject/common/CommonResultCode.java
@@ -2,7 +2,9 @@ package com.ddimtech.sbproject.common;
 
 import lombok.Getter;
 
+// 이 어노테이션은 Lombok 라이브러리에서 제공하는 기능으로, 클래스 내의 모든 필드에 대해 자동으로 getter 메서드를 생성
 @Getter
+// 고정된 상수 집합 정의
 public enum CommonResultCode {
     /**
      * 성공
@@ -20,6 +22,7 @@ public enum CommonResultCode {
      */
     INTERNAL_SERVER_ERROR(500, "there was a problem with data processing, contact the administrator.");
 
+    // resultCode와 message라는 두 개의 필드를 갖는다.
     private final int resultCode;
     private final String message;
 
