02f39082e354b9c3e93c80e05f4e9d8e34ec0560
sbdyana <tnqls9301@naver.com>
2024-08-27 18:05:37 +0900

    CommonResult 정리완료

--- src/main/java/com/ddimtech/sbproject/common/CommonResultCode.java
+++ src/main/java/com/ddimtech/sbproject/common/CommonResultCode.java
@@ -2,13 +2,15 @@ package com.ddimtech.sbproject.common;
 
 import lombok.Getter;
 
-// 이 어노테이션은 Lombok 라이브러리에서 제공하는 기능으로, 클래스 내의 모든 필드에 대해 자동으로 getter 메서드를 생성
 @Getter
-// 고정된 상수 집합 정의
+// 열거형 정의(상수들의 집합)
+// API 응답 상태코드와 메세지 정의
+// enum 클래스는 싱글톤 패턴을 구현하는 데 사용됨. 이는 enum 클래스 내에서 정의된 상수가 애플리케이션 전체에서 단 하나의 인스턴스만 존재함을 보장하기 때문.
 public enum CommonResultCode {
     /**
      * 성공
      */
+    // 성공을 나타내는 상수로, HTTP 상태 코드와 success 메시지를 갖는다.
     SUCCESS(200, "success"),
 
     /**
@@ -22,10 +24,13 @@ public enum CommonResultCode {
      */
     INTERNAL_SERVER_ERROR(500, "there was a problem with data processing, contact the administrator.");
 
-    // resultCode와 message라는 두 개의 필드를 갖는다.
+    // final로 초기되어 있는 필드들 > 한 번 초기화된 후에는 값이 변경되지 않는다.
     private final int resultCode;
     private final String message;
 
+    // 생성자 private
+    // 싱글톤 패턴은 클래스의 인스턴스를 하나만 생성하도록 제한하는 디자인 패턴으로 이를 위해 생성자를 private으로 선언하고 클래스 내부에서 단 하나의 인스턴스를 생성하여 공유한다.
+    // 이런 경우 외부에서 new CommonResultCode()를 호출할 수 없고
     private CommonResultCode(int resultCode, String message) {
         this.resultCode = resultCode;
         this.message = message;
