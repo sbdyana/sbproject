e2fc05dd8f538bcc49af061e1370cd99a6a82e45
sbdyana <tnqls9301@naver.com>
2024-08-27 14:21:43 +0900

    PageVo 완료

--- src/main/java/com/ddimtech/sbproject/common/vo/PageVo.java
+++ src/main/java/com/ddimtech/sbproject/common/vo/PageVo.java
@@ -22,10 +22,15 @@ public class PageVo {
         this.totalPages = (int) Math.ceil((double) total / (double) pageSize);
     }
 
+    // Service List에서 호출
     public PageVo(PageRequestDto pageRequestDto, int total) {
+        // 요청값 page
         this.page = pageRequestDto.getPage();
+        // 요청값 pageSize
         this.pageSize = pageRequestDto.getPageSize();
+        // list 전체값
         this.total = total;
+        //total 0이면 > 0 / 0이 아니면 있다는 거니깐 전체를 / 페이지 퍼로 나누기
         this.totalPages = total == 0 ? 0 : (int) Math.ceil((double) total / (double) pageSize);
     }
 }
